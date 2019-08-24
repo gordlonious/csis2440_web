c84187060e81c615f6d0209e06d3cf9bf3ba54f4
Gordon <gordlonious@gmail.com>
2019-08-23 21:36:02 -0600

    php hello world

---
+++ Web/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+            echo "<p> hello world </p>";
+            echo date('D F Y');
+        ?>
+    </body>
+</html>
