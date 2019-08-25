86d072660a120038f3802b9f7ba9dd6bcd9ec8d3
Gordon <gordlonious@gmail.com>
2019-08-24 23:03:42 -0600

    basic nav bar

--- Web/index.php
+++ Web/index.php
@@ -1,11 +1,34 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <style>
+            @import url(index_style.css);
+        </style>
     </head>
     <body>
-        <?php
-            echo "<p> hello world </p>";
-            echo date('D F Y');
-        ?>
+        <div class="top_container">
+            <img src="anarchy.jpg" id="anarchyjpg">
+        </div>
+        <div class="flex_container">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="link to assignment 1">Assignment 1</a>
+                    </li>
+                    <li>
+                        <a href="2">Assignment 2</a>
+                    </li>
+                    <li>
+                        <a href="ecommerce">E-Commerce</a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="php_container">
+                <?php
+                    echo "<p> HELLO WORLD </p>";
+                    echo date('D F Y');
+                ?>
+            </div>
+        </div>
     </body>
 </html>
