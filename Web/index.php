0e4f1a7165e7c5eac4dc793619e09cc2d247010c
Gordon <gordlonious@gmail.com>
2019-09-04 20:42:33 -0600

    add required introductory content for working website assignment

--- Web/index.php
+++ Web/index.php
@@ -6,28 +6,33 @@
         </style>
     </head>
     <body>
-        <div class="nav_container">
-            <nav>
-                <ul>
-                    <a href="ip address?" id="home">
-                        <img src="home.png">
-                    </a>
-                    <li>
-                        <a href="link to assignment 1">Assignment 1</a>
-                    </li>
-                    <li>
-                        <a href="2">Assignment 2</a>
-                    </li>
-                    <li>
-                        <a href="ecommerce">E-Commerce</a>
-                    </li>
-                </ul>
-            </nav>
-            <div class="php_container">
-                <?php
-                    echo "<p>PHP HELLO WORLD </p>";
-                    echo date('D F Y');
-                ?>
+        <div class="navintro_container">
+            <div class="nav_container">
+                <nav>
+                    <ul>
+                        <a href="ip address?" id="home">
+                            <img src="home.png">
+                        </a>
+                        <li>
+                            <a href="link to assignment 1">Assignment 1</a>
+                        </li>
+                        <li>
+                            <a href="2">Assignment 2</a>
+                        </li>
+                        <li>
+                            <a href="ecommerce">E-Commerce</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="intro_container">
+                <img src="OneCardSelfie.jpg" width="170px" height="140px">
+                <p>
+                    Hello, welcome to my CSIS 2440 landing page. My name is Gordon and I will help you navigate my CSIS 2440 adventures! For those who are interested, my hobbies include gaming, learning about natural languages, studying computers, reading comics, skateboarding, and more (that probably aren't as habitual as the ones already listed). I'm also interested in learning how to build modern educational systems, artificial intelligence units (preferably ones that pass the Turing test), and natural language research tooling. My favorite book to date is Ender's Game - a great science fiction novel. A favorite film is a bit harder for me to choose, but Pan's Labyrinth and Man of Steel are definitely in the top ten. Feel free to browse at your leisure! More content will be coming throughout the semester.
+                </p>
+                <img src="Superman_s.png" width="100px" height="140px">
+                <img src="Ender&#39sGame.jpg" width="100px" height="140px">
+                <img src="Pan&#39sLabyrinth.jpg" width="100px" height="140px">
             </div>
         </div>
     </body>
