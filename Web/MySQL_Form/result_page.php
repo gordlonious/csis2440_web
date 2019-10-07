1b77149032ef892ff454918694eee340a195570b
Gordon <gordlonious@gmail.com>
2019-10-07 01:13:54 -0600

    set form variables in result page

--- Web/MySQL_Form/result_page.php
+++ Web/MySQL_Form/result_page.php
@@ -5,5 +5,12 @@
         <title>MySQL Results</title>
     </head>
     <body>
+    <?php
+        $firstname = $_POST['first_name'];
+        $lastname = $_POST['last_name'];
+        $birthday = $_POST['birthday'];
+        $email = $_POST['email'];
+        $password = $_POST['pwd'];
+    ?>
     </body>
 </html>
