661bc09eff9916006a0cc1b62f48f11485a005f3
Gordon <gordlonious@gmail.com>
2019-10-13 06:29:39 -0600

    basic styling for form page

--- Web/MySQL_Form/form_page.php
+++ Web/MySQL_Form/form_page.php
@@ -3,32 +3,45 @@
     <head>
         <meta charset="US-ASCII">
         <title>MySQL Form</title>
+        <link rel="stylesheet" type="text/css" href="form_style.css">
     </head>
     <body>
+        <h2>Add, Update, or Search for a Player!</h2>
         <form action="result_page.php" method="post">
-            <label for="first_name">First Name</label>
-            <input type="text" id="first_name">
-
-            <label for="last_name">Last Name</label>
-            <input type="text" id="last_name">
-
-            <label for="birthday">Birthday</label>
-            <input type="date" id="birthday">
-
-            <label for="email">Email</label>
-            <input type="email" id="email">
-
-            <label for="pwd">Password</label>
-            <input type="password" id="pwd">
-
-            <label for="insert_radio">Add a new database record</label>
-            <input type="radio" name="query_type" id="insert_radio" checked="checked">
-
-            <label for="update_radio">Update an existing database record</label>
-            <input type="radio" name="query_type" id="update_radio">
-
-            <label for="search_radio">Search for an existing database record</label>
-            <input type="radio" name="query_type" id="search_radio">
+            <table>
+                <tr>
+                    <td><label for="first_name">First Name</label></td>
+                    <td><input type="text" id="first_name"></td>
+                </tr>
+                <tr>
+                    <td><label for="last_name">Last Name</label></td>
+                    <td><input type="text" id="last_name"></td>
+                </tr>
+                <tr>
+                    <td><label for="birthday">Birthday</label></td>
+                    <td><input type="date" id="birthday"></td>
+                </tr>
+                <tr>
+                    <td><label for="email">Email</label></td>
+                    <td><input type="email" id="email"></td>
+                </tr>
+                <tr>
+                    <td><label for="pwd">Password</label></td>
+                    <td><input type="password" id="pwd"></td>
+                </tr>
+                <tr>
+                    <td><label for="insert_radio">Add a new database record</label></td>
+                    <td class="radio_td"><input type="radio" name="query_type" id="insert_radio" checked="checked"></td>
+                </tr>
+                <tr> 
+                    <td><label for="update_radio">Update an existing database record</label></td>
+                    <td class="radio_td"><input type="radio" name="query_type" id="update_radio"></td>
+                </tr>
+                <tr>
+                    <td><label for="search_radio">Search for an existing database record</label></td>
+                    <td class="radio_td"><input type="radio" name="query_type" id="search_radio"></td>
+                </tr>
+            </table>
         </form>
     </body>
 </html>
