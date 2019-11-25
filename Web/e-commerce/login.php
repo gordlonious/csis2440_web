4f2ba6ed35fd1af8ae9c2f2e8c9e38db031f7216
Gordlonious <gordlonious@gmail.com>
2019-11-25 01:46:42 -0700

    super-basic styling + charset meta

--- Web/e-commerce/login.php
+++ Web/e-commerce/login.php
@@ -1,6 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
+        <meta charset="US-ASCII">
 		<title>E-Commerce Login</title>
         <script>
             function submitLogin()
@@ -21,20 +22,52 @@
                 signupForm.removeAttribute('hidden');
             }
         </script>
+        <style>
+            h2 {
+                text-align: center;
+            }
+
+            form > input {
+                display: block;
+                margin-bottom: 10px;
+            }
+
+            body > form {
+                margin: auto;
+                width: 200px;
+            }
+
+            body > p {
+                text-align: center;
+            }
+
+            input[type="button"] {
+                width: 75px;
+                height: 25px;
+            }
+
+            button {
+                background: none;
+                border-style: solid;
+                border-width: 1px;
+                border-color: black;
+            }
+        </style>
 	</head>
     <body>
+        <h2>The Anvil N' Stuff Store</h2>
         <form action="catalogue.php" id="login_form">
             <label for="uname_input">Username:</label>
-            <input type="text" name="uname_input"/>
+            <input type="text" id="uname_input"/>
             <label for="pwd_input">Password:</label>
-            <input type="password" name="pwd_input"/>
+            <input type="password" id="pwd_input"/>
             <input type="button" onclick="submitLogin()" value="Login"/>
         </form>
         <p>Don't have an account yet? <button onclick="revealSignup()">Click here to signup!</button></p>
         <form action="login.php" id="signup_form" hidden>
-            <label for="new_uname_input">Username:</label>
+            <label for="new_uname_input">New Username:</label>
             <input type="text" name="new_uname_input"/>
-            <label for="new_pwd_input">Password:</label>
+            <label for="new_pwd_input">New Password:</label>
             <input type="password" name="new_pwd_input"/>
             <input type="button" onclick="submitSignup()" value="Signup"/>
         </form>
