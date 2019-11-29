446289b05d511b5b70dfe5b0cd3c85959991ceb3
Gordlonious <gordlonious@gmail.com>
2019-11-28 20:08:00 -0700

    initial create account class

---
+++ Web/e-commerce/CreateAccount.php
@@ -0,0 +1,78 @@
+<?php
+require_once dirname(__DIR__).'../MySQL_Form/database.php';
+
+class CreateAccount extends Database
+{
+    private $mysql;
+
+    function __construct($user, $pwdfilepath)
+    {
+        parent::__construct($user, $pwdfilepath);
+
+        $mysql = parent::getdbconnection();
+
+        $this->create_commerce_user_table();
+    }
+
+    private function create_commerce_user_table()
+    {
+        $sql =
+        "
+            CREATE TABLE IF NOT EXISTS `CSIS2440`.`CommerceUser`
+            (
+                commerceUserId INT,
+                commerceUsername VARCHAR(255),
+                commercePwdHash CHAR(255),
+                PRIMARY KEY (commerceUserId)
+            );
+        ";
+
+        $sqlResult = $this->mysql->query($sql);
+
+        if (!$sqlResult)
+        {
+            echo "ERROR WITH CREATE ACCOUNT TABLE";
+        }
+    }
+
+    public function insert_commerce_user($user, $pwd)
+    {
+        $pwdHash = password_hash($pwd, PASSWORD_DEFAULT);
+
+        $sql = 
+        "
+            INSERT INTO `CSIS2440`.`CommerceUser`
+            (
+                commerceUsername,
+                commercePwdHash
+            )
+            VALUES
+            (
+                ?,
+                ?
+            );
+        ";
+
+        $stmnt = $this->mysql->prepare($sql);
+
+        if (!$stmnt)
+        {
+            echo "ERROR WITH PREPARING CREATE ACCOUNT STATEMENT";
+        }
+
+        $bindResult = $stmnt->bind_param("ss", $user, $pwdHash);
+
+        if (!$bindResult)
+        {
+            echo "ERROR WITH CREATE ACCOUNT BINDING";
+        }
+
+        $executionResult = $stmnt->execute();
+
+        if (!$executionResult)
+        {
+            echo "ERROR WITH CREATE ACCOUNT INSERT";
+        }
+    }
+}
+?>
