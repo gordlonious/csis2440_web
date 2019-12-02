7bbf1a277dcb8ae34ac4480b061fe2aab456990a
Gordlonious <gordlonious@gmail.com>
2019-12-01 18:25:00 -0700

    php script to get catalogue product options

--- Web/e-commerce/catalogue.php
+++ Web/e-commerce/catalogue.php
@@ -6,7 +6,36 @@
     <body>
         <h3>Please select an item!</h3>
         <select id="product_select">
-            <!-- use php to get product options -->
+            <?php
+            require_once '../MySQL_Form/database.php';
+            $pwdfilepath = '/var/www/site_credentials/mysql_web_pwd';
+            $db = new Database('web', $pwdfilepath);
+
+            $con = $db->getdbconnection();
+
+            $sql =
+            "
+            SELECT `name`
+            FROM `CSIS2440`.`Product`
+            ";
+
+            $sqlResult = $con->query($sql);
+
+            if (!$sqlResult)
+            {
+                http_response_code(500);
+            }
+
+            foreach ($sqlResult as $row)
+            {
+                $productName = $row['name'];
+                echo "<option>$productName</option>";
+            }
+
+            $sqlResult->free();
+
+            $con->close();
+            ?>
         </select>
     </body>
 </html>
